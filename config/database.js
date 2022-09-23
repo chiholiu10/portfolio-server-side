8c7c8076613f137c5ad970433387ff540e4b482f
chiho <chiholiu10@gmail.com>
2022-09-23 13:15:33 +0200

    config production

--- config/database.js
+++ config/database.js
@@ -1,11 +1,28 @@
 const path = require('path');
+const { config } = require('process');
 
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'sqlite',
+module.exports = ({ env }) => {
+  if ('NODE_ENV' === 'production') {
+    return {
+      connection: {
+        client: 'postgres',
+        connection: {
+          host: config.host,
+          port: config.port,
+          database: config.database,
+          user: config.user,
+          password: config.password
+        }
+      }
+    };
+  }
+  return {
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      client: 'postgres',
+      connection: {
+        filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      },
+      useNullAsDefault: true,
     },
-    useNullAsDefault: true,
-  },
-});
+  };
+};

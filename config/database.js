44da1621088bcf4905188830beda8a8020dc250b
chiho <chiholiu10@gmail.com>
2022-09-24 03:35:26 +0200

    update file

--- config/database.js
+++ config/database.js
@@ -1,11 +1,32 @@
 const path = require('path');
-
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'sqlite',
+const parse = require('pg-connection-string').parse;
+const { config } = parse(process.env.DATABASE_URL);
+console.log(config);
+module.exports = ({ env }) => {
+  if (env('NODE_ENV') === 'production') {
+    return {
+      connection: {
+        client: 'postgres',
+        connection: {
+          host: config.host,
+          port: config.port,
+          database: config.database,
+          user: config.user,
+          password: config.password
+        },
+        ssl: {
+          rejectUnauthorized: false,
+        }
+      },
+    };
+  }
+  return {
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      client: 'sqlite',
+      connection: {
+        filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      },
+      useNullAsDefault: true,
     },
-    useNullAsDefault: true,
-  },
-});
+  };
+};

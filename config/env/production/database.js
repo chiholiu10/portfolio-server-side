c4f50ecaacc65aac416e1f7c122564a074c91b47
chiho <chiholiu10@gmail.com>
2022-09-25 03:41:03 +0200

    new update

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -19,15 +19,16 @@ module.exports = ({ env }) => {
           },
         },
         debug: false,
-      };
-    };
-    return {
-      connection: {
-        client: 'sqlite',
-        connection: {
-          filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
-        },
-        useNullAsDefault: true,
       },
     };
+  }
+  return {
+    connection: {
+      client: 'sqlite',
+      connection: {
+        filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      },
+      useNullAsDefault: true,
+    },
   };
+};

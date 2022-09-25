0ced56252cf29bfe8531a9d5e6da03aaec9caa05
chiho <chiholiu10@gmail.com>
2022-09-25 04:33:21 +0200

    update api salt token

--- config/admin.js
+++ config/admin.js
@@ -1,4 +1,7 @@
 module.exports = ({ env }) => ({
+  apiToken: {
+    salt: env('API_TOKEN_SALT', '39d36fe91efbd15e7dfe14c76313379f'),
+  },
   auth: {
     secret: env('ADMIN_JWT_SECRET', 'c328c383da17a6ea6aed8fe58863c374'),
   },

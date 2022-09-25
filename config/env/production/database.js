39abdd1c2fa8bec2d2e380d70f037ef233844a90
chiho <chiholiu10@gmail.com>
2022-09-25 03:39:34 +0200

    new update

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,32 +1,33 @@
 const path = require('path');
 const parse = require('pg-connection-string').parse;
-const { config } = parse(process.env.DATABASE_URL);
-console.log(config);
+const config = parse(process.env.DATABASE_URL);
+
 module.exports = ({ env }) => {
   if (env('NODE_ENV') === 'production') {
     return {
       connection: {
         client: 'postgres',
         connection: {
-          host: config.host,
-          port: config.port,
-          database: config.database,
-          user: config.user,
-          password: config.password
+          host: env('DATABASE_HOST', '127.0.0.1'),
+          port: env.int('DATABASE_PORT', 5432),
+          database: env('DATABASE_NAME', 'strapi'),
+          user: env('DATABASE_USERNAME', 'strapi'),
+          password: env('DATABASE_PASSWORD', 'strapi'),
+          schema: env('DATABASE_SCHEMA', 'public'), // Not required
+          ssl: {
+            rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+          },
         },
-        ssl: {
-          rejectUnauthorized: false,
-        }
+        debug: false,
+      };
+    };
+    return {
+      connection: {
+        client: 'sqlite',
+        connection: {
+          filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+        },
+        useNullAsDefault: true,
       },
     };
-  }
-  return {
-    connection: {
-      client: 'sqlite',
-      connection: {
-        filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
-      },
-      useNullAsDefault: true,
-    },
   };
-};

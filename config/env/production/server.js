40f60eea79af53e9a048ae6285c2ff3d04642f51
Chiho <chiho@Chihos-MacBook-Pro.local>
2022-10-24 19:33:47 +0200

    change server env

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
-  url: env('https://portfolioserverside.herokuapp.com'),
+  url: env(MY_HEROKU_URL),
   proxy: true,
   app: {
-    key: env.array('APP_KEYS', ['SDrv5PIa5w9OfJlUUITNaQ==,RNmhohRP/7PVZq/IPfe/aw==,9RlKrzx+61wyupmLBxuPjw==,AKUnfz4BzfC3iew8ocvdIg=='])
+    key: env.array('APP_KEYS', [APP_KEYS])
   }
 })
